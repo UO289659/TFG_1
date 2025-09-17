8e90f548d43e9977f478af65589e9c6991a29451
UO289659 <uo289659@uniovi.es>
2025-09-17 11:54:35 +0200

    fix nombre variable

--- backend/user-service/server.js
+++ backend/user-service/server.js
@@ -19,8 +19,9 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+
 mongoose
-  .connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+  .connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => console.log("✅ [Auth Service] Conectado a MongoDB"))
   .catch((err) => console.error("❌ [Auth Service] Error al conectar:", err));
 /**
